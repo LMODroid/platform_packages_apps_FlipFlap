232598c74805998ea87f66de4924d09beb4041f2
Bruno Martins <bgcngm@gmail.com>
2017-10-06 14:32:59 +0100

    FlipFlap: Build with AAPT2
    Change-Id: I58ee547ff1dd115bff65925e05ce5530ddfe6290

--- Android.mk
+++ Android.mk
@@ -3,12 +3,16 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES :=  \
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v14-preference \
     android-support-v13 \
     android-support-v7-appcompat \
     android-support-v7-preference \
     android-support-v7-recyclerview \
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
@@ -21,17 +25,7 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(LOCAL_PATH)/../../../packages/resources/devicesettings/res \
-    frameworks/support/v14/preference/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/preference/res \
-    frameworks/support/v7/recyclerview/res
-
-LOCAL_AAPT_FLAGS := --auto-add-overlay
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v14.preference
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.preference
-LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.recyclerview
+    $(TOP)/packages/resources/devicesettings/res
 
 include $(BUILD_PACKAGE)
 
