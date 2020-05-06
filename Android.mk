d629143142503bb0a53afee974d9092fc109a2df
Michael Bestas <mkbestas@lineageos.org>
2020-05-06 20:14:32 +0300

    FlipFlap: Add privapp permissions xml
    Change-Id: Ib6f52a2e97651f0ab3515c85e29690a829804dc3

--- Android.mk
+++ Android.mk
@@ -26,4 +26,14 @@ LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
 
+LOCAL_REQUIRED_MODULES := privapp_whitelist_org.lineageos.flipflap.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_org.lineageos.flipflap.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
